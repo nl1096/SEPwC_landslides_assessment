91eb52daa85d523a8a7a59fd632b7da082b450c4
Natalia Lukomska <nl1096@york.ac.uk>
2025-05-27 15:05:17 +0100

    Extract raster values from landslide points

--- terrain_analysis.R
+++ terrain_analysis.R
@@ -1,63 +1,18 @@
-#!/usr/bin/env Rscript
+# Required packages: argparse, terra, randomForest
+# Optional: ggplot2
+
+# Load libraries
 suppressPackageStartupMessages({
-library(argparse)
+  library(argparse)
+  library(terra)
+  library(randomForest)
+  library(ggplot2)
 })
 
-extract_values_from_raster<-function(raster_stack, shapefile) {
+# Suppress global variable warnings for ggplot aesthetics
+utils::globalVariables(c("x", "y", "prob", "Variable", "MeanDecreaseGini"))
 
-    return()
-
-}
-
-
-create_dataframe<-function(raster_stack, shapefile, landslide) {
-
-    return()
-
-}
-
-make_classifier<-function(dataframe) {
-    return()
-}
-
-make_probability_raster<-function(raster_stack, classifier) {
-
-    return()
-}
-
-
-main <- function(args) {
-
-}
-
-if(sys.nframe() == 0) {
-
-    # main program, called via Rscript
-    parser = ArgumentParser(
-                    prog="Landslide Risk",
-                    description="Calculate landslide probability risk using Random Forests"
-                    )
-    parser$add_argument('--topography',
-                    required=T,
-                    help="topographic raster file")
-    parser$add_argument('--geology',
-                    required=T,
-                    help="geology raster file")
-    parser$add_argument('--landcover',
-                    required=T,
-                    help="landcover raster file")
-    parser$add_argument('--faults',
-                    required=T,
-                    help="fault location shapefile")
-    parser$add_argument("landslides",
-                    help="the landslide location shapefile")
-    parser$add_argument("output",
-                    help="the output raster file")
-    parser$add_argument('-v', '--verbose',
-                    action='store_true',
-                    default=F,
-                    help="Print progress")
-
-    args = parser$parse_args()  
-    main(args)
+# Extract raster values from landslide points
+extract_values_from_raster <- function(raster_stack, shapefile) {
+  terra::extract(raster_stack, shapefile)
 }
