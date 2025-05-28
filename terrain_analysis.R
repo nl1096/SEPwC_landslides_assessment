d37e61d6231c5eae267422d7a75a746b8e4d6978
Natalia Lukomska <nl1096@york.ac.uk>
2025-05-28 20:12:30 +0100

    Complete pipeline for landslide analysis

--- terrain_analysis.R
+++ terrain_analysis.R
@@ -33,3 +33,48 @@ make_classifier <- function(dataframe) {
 make_probability_raster <- function(raster_stack, classifier) {
   terra::predict(raster_stack, classifier, type = "prob", index = 2)
 }
+
+main <- function(args) {
+  # Load all raster and vector data
+  topo <- terra::rast(args$topography)
+  geology <- terra::rast(args$geology)
+  landcover <- terra::rast(args$landcover)
+  faults <- suppressWarnings(terra::vect(args$faults))
+  landslides <- suppressWarnings(terra::vect(args$landslides))
+
+  # Derive slope and distance from faults
+  slope <- terra::terrain(topo, v = "slope", unit = "degrees")
+  template <- topo
+  values(template) <- NA
+  faults_raster <- terra::rasterize(faults, template, field = 1)
+  dist <- terra::distance(faults_raster)
+
+  # Stack rasters
+  stack <- c(topo, geology, landcover, slope, dist)
+
+  # Create training data
+  pos_df <- create_dataframe(stack, landslides, 1)
+  set.seed(123)
+  bg_points <- terra::spatSample(
+    stack,
+    size = nrow(pos_df),
+    method = "random",
+    as.points = TRUE,
+    na.rm = TRUE
+  )
+  neg_df <- create_dataframe(stack, bg_points, 0)
+
+  # Combine and train
+  df <- rbind(pos_df, neg_df)
+  model <- make_classifier(df)
+
+  # Predict probability map
+  prob_map <- make_probability_raster(stack, model)
+  terra::writeRaster(prob_map, args$output, overwrite = TRUE)
+
+  # Print model summary if verbose
+  if (!is.null(args$verbose) && isTRUE(args$verbose)) {
+    print("Training completed")
+    print(model)
+  }
+}
